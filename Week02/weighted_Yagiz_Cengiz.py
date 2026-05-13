0eda9db7a1abc911fc7f7dfc89f7bb4946d22d76
yagizcngz <yagizcengiz888@gmail.com>
2026-05-13 17:44:46 +0300

    Update weighted_Yagiz_Cengiz.py

--- Week02/weighted_Yagiz_Cengiz.py
+++ Week02/weighted_Yagiz_Cengiz.py
@@ -1,9 +1,7 @@
 import random
-def weighted_srs(data, n, weights=None, with_replacement=False):
-    if not weights: return random.choices(data, k=n) if with_replacement else random.sample(data, n)
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    d, w, res = list(data), list(weights), []
-    for _ in range(n):
-        i = random.choices(range(len(d)), weights=w, k=1)[0]
-        res.append(d.pop(i)); w.pop(i)
-    return res
+
+def weighted_srs(data, n, weights, with_replacement=False):
+    use_choices = bool(with_replacement or weights)
+    if use_choices:
+        return random.choices(data, weights=weights, k=n)
+    return random.sample(data, n)
